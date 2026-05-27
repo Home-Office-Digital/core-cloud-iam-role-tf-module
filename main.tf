de332702d72cb25ba1b8cbe78fecef081625dd4b
Michael Frost <michael.frost@digital.homeoffice.gov.uk>
2026-05-27 10:15:37 +0100

    chore(CCL-9103): update org refs (#21)
    * chore(CCL-9103): update org refs
    
    * chore(CCL-9103): update org refs
    
    * chore(CCL-9103): update org refs
    
    * chore(CCL-9103): revert action refs to UKHomeOffice (not yet migrated)
    
    * chore(CCL-9103): update terraform version to fix provider signature verification
    
    * added checks for false postives

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ locals {
 }
 
 module "iam_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role" #checkov:skip=CKV_TF_1:Terraform Registry module - version is pinned via the version attribute
   version = "6.2.1"
 
   tags = merge(local.common_tags)
